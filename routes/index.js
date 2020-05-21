ae1750adcc011442a49b37e6bbc101861b560abb
Zuoxiaoxian <1980179070@qq.com>
2020-05-21 11:08:34 +0800

    updata POST of index

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var router = express();
 const bodyparser = require('body-parser');
 
 /* index api */
-/*
+
 router.get('/', function(req, res, next){
     // json
     var code = { code: 200, message: "index" }
@@ -16,16 +16,21 @@ router.get('/', function(req, res, next){
     };
     res.send(JSON.stringify(indexJson))
 });
-*/
+
 
 // app.use使用中间机、插件, application/json 解析json
 router.use(bodyparser.json());
 router.use(bodyparser.urlencoded({ extended: false }));
 
+
+
+// curl -d "name=123"  -X POST  http://localhost:5000/
 router.post('/', (req, res)=>{
     // 接收post 参数
     // express 不能直接解析消息体、请求体，需要第三方插件body-parser
-    console.log(req.body);
+    console.dir(req.body)
+    var msg = req.body;
+    console.log(JSON.stringify(msg));
     res.send(req.body);
 });
 
